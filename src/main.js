ece02ec43e5b38efb50d9cb533b28ae5dc24549b
Hamza <hamzazaidi253@gmail.com>
2026-02-27 14:14:33 +0000

    ajouter partie de calories

--- src/main.js
+++ src/main.js
@@ -2,23 +2,38 @@ import { getRecipes } from "./api/recipeProvider.js";
 const app = document.getElementById("app") ;
 function  displayRecipes(recipes) {
     app.innerHTML = "";
-    recipes.forEach((recipe)=>{
-       const card = document.createElement("div");
-    card.classList.add("card");
+   for(let i= 0;i<recipes.length;i++){
 
-        card.innerHTML =`
-           <img src="${recipe.image}" />
-           <h3> ${recipe.name}</h3>
-            <p>${recipe.prepTimeMinutes} min</p>
+      let color = "";
+     
+if(recipes[i].caloriesPerServing < 400){
+    color = "green";
+}else if (recipes[i].caloriesPerServing >= 400  && recipes[i].caloriesPerServing < 800 ){
+    color = "orange";
+}else {
+  color = "red";
+}
+       const card = document.createElement("div");
+   
+
+        card.innerHTML +=`
+           <img src="${recipes[i].image}" />
+           <h3>${recipes[i].name}</h3>
+           <h2 style = "color :${color}"> ${recipes[i].caloriesPerServing} kcal</h2>
+           
+            <p>${recipes[i].prepTimeMinutes} min</p>
+
      `;    
        app.appendChild(card);    
     }
 
-    )
-}
+   }
+
 async function init() {
   const recipes = await getRecipes();
   displayRecipes(recipes);
 }
 
 init();
+
+
--- src/services/calorieService.js
+++
@@ -1,8 +0,0 @@
-let badgeColor = "";
-if(recipes.caloriesPerServing < 400){
-    badgeColor = "green";
-}else if (recipes.caloriesPerServing <= 800){
-    badgeColor = "orange";
-}else {
-  badgeColor = "red";
-}
