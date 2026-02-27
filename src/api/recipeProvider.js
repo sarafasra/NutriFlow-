b7a2e01a69a748d8c77facf4efb19653dd077c03
Hamza <hamzazaidi253@gmail.com>
2026-02-27 14:16:50 +0000

    ajouter la function getRecipes

--- src/api/recipeProvider.js
+++ src/api/recipeProvider.js
@@ -1,13 +1,1 @@
-export async function getRecipes() {
-    try{
-        const fetchdata =await fetch("https://dummyjson.com/recipes");
-        const data =await fetchdata.json()
-        console.log(data);
-        return data.recipes;
-        
-    }catch(error){
-       console.log("error", error);
-        
-    }
-}  
 
